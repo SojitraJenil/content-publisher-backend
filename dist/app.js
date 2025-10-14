73de1f7910cfdfc8be8414a6fedb600a0554477c
SojitraJenil <jenilsojitra4@gmail.com>
2025-10-14 23:42:29 +0530

    url change

--- dist/app.js
+++ dist/app.js
@@ -13,7 +13,7 @@ dotenv_1.default.config();
 (0, db_1.default)();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: 'https://content-publisher-backend-blfn.onrender.com',
+    origin: 'content-publisher-assessment.vercel.app',
     credentials: true
 }));
 app.use(express_1.default.json());
