673e849de662cb3c23250f6455877f7e852703c7
SojitraJenil <jenilsojitra4@gmail.com>
2025-10-14 23:56:06 +0530

    fixed

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,22 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+const allowedOrigins = [
+    'http://localhost:3000', // React dev server
+    'https://content-publisher-assessment.vercel.app' // Production frontend
+];
+
 app.use(cors({
-    origin: 'https://content-publisher-assessment.vercel.app',
+    origin: function (origin, callback) {
+        // allow requests with no origin (like Postman)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
 
