266cd03d706451b495d9f8306da2555e6f4daa51
SojitraJenil <jenilsojitra4@gmail.com>
2025-10-15 00:11:52 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-app.use(cors({
-    origin: 'https://content-publisher-assessment.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
