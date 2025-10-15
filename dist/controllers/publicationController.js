387611a52abf2bba1de90a3e7a38303210c9ecad
SojitraJenil <jenilsojitra4@gmail.com>
2025-10-15 11:47:15 +0530

    add build due to fail user profile api

--- dist/controllers/publicationController.js
+++ dist/controllers/publicationController.js
@@ -12,13 +12,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchPublications = exports.deletePublication = exports.updatePublication = exports.createPublication = exports.getPublications = void 0;
+exports.deletePublication = exports.updatePublication = exports.createPublication = exports.getUserProfile = exports.getPublications = void 0;
 const Publication_1 = __importDefault(require("../models/Publication"));
+const User_1 = __importDefault(require("../models/User"));
 const getPublications = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const publications = yield Publication_1.default.find({ user: req.user._id });
     res.json(publications);
 });
 exports.getPublications = getPublications;
+const getUserProfile = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const user = yield User_1.default.findById(req.user.id).select('-password');
+        if (!user)
+            return res.status(404).json({ message: 'User not found' });
+        res.json(user);
+    }
+    catch (error) {
+        console.error('Profile error:', error.message);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+exports.getUserProfile = getUserProfile;
 const createPublication = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { title, content, status } = req.body;
     const publication = yield Publication_1.default.create({
@@ -45,47 +59,18 @@ const updatePublication = (req, res) => __awaiter(void 0, void 0, void 0, functi
 });
 exports.updatePublication = updatePublication;
 const deletePublication = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const publication = yield Publication_1.default.findById(req.params.id);
-    if (!publication)
-        return res.status(404).json({ message: 'Publication not found' });
-    if (publication.user.toString() !== req.user._id.toString())
-        return res.status(403).json({ message: 'Not authorized' });
-    yield publication.remove();
-    res.json({ message: 'Publication deleted' });
-});
-exports.deletePublication = deletePublication;
-const searchPublications = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { query, status, sort = 'desc', page = 1, limit = 10 } = req.query;
-        const filter = { user: req.user._id };
-        if (query) {
-            filter.$or = [
-                { title: { $regex: query, $options: 'i' } },
-                { content: { $regex: query, $options: 'i' } },
-            ];
-        }
-        if (status) {
-            filter.status = status;
-        }
-        const pageNum = parseInt(page) || 1;
-        const limitNum = parseInt(limit) || 10;
-        const skip = (pageNum - 1) * limitNum;
-        const sortOrder = sort === 'asc' ? 1 : -1;
-        const publications = yield Publication_1.default.find(filter)
-            .sort({ createdAt: sortOrder })
-            .skip(skip)
-            .limit(limitNum);
-        const total = yield Publication_1.default.countDocuments(filter);
-        res.json({
-            total,
-            page: pageNum,
-            pages: Math.ceil(total / limitNum),
-            results: publications,
-        });
+        const publication = yield Publication_1.default.findById(req.params.id);
+        if (!publication)
+            return res.status(404).json({ message: 'Publication not found' });
+        if (publication.user.toString() !== req.user._id.toString())
+            return res.status(403).json({ message: 'Not authorized' });
+        yield publication.deleteOne();
+        res.json({ message: 'Publication deleted' });
     }
     catch (error) {
-        console.error('Search error:', error.message);
+        console.error('Delete error:', error.message);
         res.status(500).json({ message: 'Server error' });
     }
 });
-exports.searchPublications = searchPublications;
+exports.deletePublication = deletePublication;
--- dist/routes/authRoutes.js
+++ dist/routes/authRoutes.js
@@ -5,7 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const authController_1 = require("../controllers/authController");
+const authMiddleware_1 = require("../middleware/authMiddleware");
+const publicationController_1 = require("../controllers/publicationController");
 const router = express_1.default.Router();
 router.post('/signup', authController_1.signup);
 router.post('/login', authController_1.login);
+router.get('/user', authMiddleware_1.protect, publicationController_1.getUserProfile);
 exports.default = router;
